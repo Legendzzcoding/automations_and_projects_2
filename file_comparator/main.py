42924302c35f1e708b84e3dd174e01e799f86daf
Legendzzcoding <yashanant8@gmail.com>
2024-07-28 11:43:38 +0530

    Update main.py

--- file_comparator/main.py
+++ file_comparator/main.py
@@ -9,40 +9,59 @@ def index():
     processed_text=""
     diff_lines1 = []
     diff_lines2 = []
-    color_line1=[]
-    color_line2=[]
     check_if_diff_ans_has_value=''
     if request.method=='POST':
         user_input1=request.form['text_input1']
         user_input2=request.form['text_input2']
         f1=io.StringIO(user_input1).readlines()
         f2=io.StringIO(user_input2).readlines()
+        if(len(f1)>len(f2)):
+            dif=len(f1)-len(f2)
+            i=1
+            while(i<=1):
+                user_input2=user_input2+"\r\n"
+                i=i+1
+            f2=io.StringIO(user_input2).readlines()
+        else:
+            dif=len(f2)-len(f1)
+            i=1
+            while(i<=1):
+                user_input1=user_input1+"\r\n"
+                i=i+1
+            f1=io.StringIO(user_input1).readlines()
         check_if_diff_ans_has_value=''
         differ = difflib.Differ()
+        ans=open("ans.txt","w")#
+        s1="First file-->"+repr(user_input1)#
+        s2="Second file-->"+repr(user_input2)
+        ans.write(s1)#
+        ans.write(s2)#
         diff_lines1 = []
         diff_lines2 = []
-        color_line1=[]
-        color_line2=[]
+        fa=open("output1.txt","w")#
+        fb=open("output2.txt","w")#
+        fus1=open("user_input1.txt","w")#
+        fus2=open("user_input2.txt","w")#
         for line in differ.compare(f1, f2):
             marker=line[0]
             if marker == " ":
                 diff_lines1.append(line[2:])
                 diff_lines2.append(line[2:])
-                color_line1.append("blue")
-                color_line2.append("blue")
+                fa.write(line[2:]+"\n")#
+                fb.write(line[2:]+"\n")#
             elif marker == "-":
                 diff_lines1.append(f'{line[2:]}')
-                diff_lines2.append("-\n")
-                color_line1.append("green")
-                color_line2.append("red")
+                diff_lines2.append("-")
+                fa.write(line[2:]+"\n")#
+                fb.write("-\n")#
             elif marker == "+":
-                diff_lines1.append("-\n")
+                diff_lines1.append("-")
                 diff_lines2.append(f'{line[2:]}')
-                color_line1.append("red")
-                color_line2.append("green")
+                fa.write("-\n")#
+                fb.write(line[2:]+"\n")#
             check_if_diff_ans_has_value='a'
         
-    return render_template("index.html",check_if_diff_ans_has_value=check_if_diff_ans_has_value,diff_lines2=diff_lines2,diff_lines1=diff_lines1,color_line1=color_line1,color_line2=color_line2)
+    return render_template("index.html",check_if_diff_ans_has_value=check_if_diff_ans_has_value,diff_lines2=diff_lines2,diff_lines1=diff_lines1)
 
 if __name__ == "__main__":
     app.run(debug=True)
